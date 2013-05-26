493a4b35ef016f3ab3476f68320090adeac7c0d5
Kendrick Ledet <kendrickledet@gmail.com>
2013-05-26 00:24:18 -0500

    added _new_message ajax route

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request, jsonify, Response
+#from flask.ext.security.datastore.mongoengine import MongoEngineUserDatastore
+#from models import *
 import os
+
 app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {'DB': "autocraig"}
+app.config["SECRET_KEY"] = "k3l432l3lj"
+
+#db = MongoEngine(app)
 
 @app.route('/')
 def index():
@@ -18,6 +25,12 @@ def go():
 def messages():
 	return render_template('messages.html')
 
+@app.route('/_new_message')
+def new_message():
+	fromAddress = request.args.get('fromAddress', 0, type=str)
+	print fromAddress
+
+	return jsonify(t=fromAddress)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
