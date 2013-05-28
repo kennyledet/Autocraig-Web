3c0a62bdb687d5c8f97e1a1bd9cdcd47e6351307
Kendrick Ledet <kendrickledet@gmail.com>
2013-05-27 21:18:07 -0500

    various improvements, db/model fixes, ajax flow fixes, added modal for choosing msgs

--- app.py
+++ app.py
@@ -1,63 +1,42 @@
 from flask import Flask, render_template, request, jsonify, Response
-from flask.ext.sqlalchemy import SQLAlchemy
-
-import datetime
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/data.db'
-db  = SQLAlchemy(app)
 
-# Models
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    created_at  = db.Column(db.DateTime())
-
-    toAddress   = db.Column(db.String(255))
-    fromAddress = db.Column(db.String(255))
-    subject     = db.Column(db.String(255))
-    body        = db.Column(db.Text())
-
-    reportsEnabled = db.Column(db.Boolean())
-
-    def __init__(self, toAddress, fromAddress, subject, body, reportsEnabled):
-    	self.created_at  = datetime.datetime.now()
-        self.toAddress   = toAddress
-        self.fromAddress = fromAddress
-        self.subject     = subject
-        self.body 		 = body
-        self.reportsEnabled = reportsEnabled
-
-    def __repr__(self):
-        return '<Subject %r>' % self.subject
+import models
 
 # Routes
 @app.route('/')
 def index():
-	return render_template('index.html')
-
-@app.route('/go')
-def go():
-	urls = request.args.get('urls', 0, type=str)
-	#print urls.split('\n')
-
-	return jsonify(result=1)
+    return render_template('index.html')
 
 @app.route('/messages')
 def messages():
-	return render_template('messages.html')
+    return render_template('messages.html')
+
+@app.route('/_go')
+def go():
+    urls = request.args.get('urls', 0, type=str)
+    return jsonify()
 
 @app.route('/_new_message')
 def new_message():
-	toAddress   = request.args.get('toAddress',   0, type=str)
-	fromAddress = request.args.get('fromAddress', 0, type=str)
-	subject     = request.args.get('subject',     0, type=str)
-	body        = request.args.get('body', 		  0, type=str)
-	reportsEnabled = request.args.get('reportsEnabled', 0, type=str)
+    toAddress   = request.args.get('toAddress',   0, type=str)
+    fromAddress = request.args.get('fromAddress', 0, type=str)
+    subject     = request.args.get('subject',     0, type=str)
+    body        = request.args.get('body',        0, type=str)
+    reportsEnabled = request.args.get('reportsEnabled', 0, type=str) == 'on'
 
-
-
-	return jsonify(result=result)
+    try:
+        message = models.Message(toAddress, fromAddress, subject, body, reportsEnabled)
+        models.db.session.add(message)
+        models.db.session.commit()
+    except Exception, e:
+        result = 0
+    else:
+        result = 1
+    finally:
+        return jsonify(result=result)
 
 
 
---
+++ models.py
@@ -0,0 +1,30 @@
+import datetime
+from flask.ext.sqlalchemy import SQLAlchemy
+
+# Database config and init
+from app import app
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/data.db'
+db  = SQLAlchemy(app)
+
+# Models
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created_at  = db.Column(db.DateTime())
+
+    toAddress   = db.Column(db.String(255))
+    fromAddress = db.Column(db.String(255))
+    subject     = db.Column(db.String(255))
+    body        = db.Column(db.Text())
+
+    reportsEnabled = db.Column(db.Boolean())
+
+    def __init__(self, toAddress, fromAddress, subject, body, reportsEnabled):
+        self.created_at  = datetime.datetime.now()
+        self.toAddress   = toAddress
+        self.fromAddress = fromAddress
+        self.subject     = subject
+        self.body        = body
+        self.reportsEnabled = reportsEnabled
+
+    def __repr__(self):
+        return '<Subject %r>' % self.subject
